91e9b44961087dcba5c6c84f784f05ba482ed57c
Ben <benoit.zeller@gmail.com>
2024-01-09 15:06:56 +0100

    add nofollow to links

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -9,8 +9,35 @@ class ForumPost < ApplicationRecord
   scope :sorted, -> { order(:created_at) }
 
   after_update :solve_forum_thread, if: :solved?
+  before_save :add_nofollow_to_links
 
   def solve_forum_thread
     forum_thread.update(solved: true)
   end
+
+  private
+
+  def add_nofollow_to_links
+    return unless content.present?
+
+    doc = Nokogiri::HTML.fragment(content.body.to_s)
+
+    doc.css('a').each do |link|
+      href = link['href']
+      unless allowed_domain?(href)
+        link['rel'] = 'nofollow'
+      end
+    end
+
+    self.content = doc.to_html
+  end
+
+  def allowed_domain?(url)
+    allowed_domains = ['example.com', 'yourdomain.com'] # Add your allowed domains here
+    uri = URI.parse(url)
+    allowed_domains.any? { |domain| uri.host.end_with?(domain) }
+  rescue URI::InvalidURIError
+    false
+  end
+
 end
